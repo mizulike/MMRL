ff25fb934ce92a8708c1063a22661d6adf4a4a7d
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2025-03-01 18:44:34 +0100

    Update ReleaseTree.kt

--- app/src/main/kotlin/com/dergoogler/mmrl/utils/timber/ReleaseTree.kt
+++ app/src/main/kotlin/com/dergoogler/mmrl/utils/timber/ReleaseTree.kt
@@ -3,7 +3,7 @@ package com.dergoogler.mmrl.utils.timber
 import android.util.Log
 import timber.log.Timber
 
-class ReleaseTree : Timber.DebugTree() {
+class ReleaseTree : Timber.Tree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
         if (priority == Log.VERBOSE || priority == Log.DEBUG) {
             return
