3e462e3e2763445ec774d6ad4503a730b1d29b66
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2025-03-01 18:42:58 +0100

    no debug logs in prod

--- app/src/main/kotlin/com/dergoogler/mmrl/utils/timber/ReleaseTree.kt
+++ app/src/main/kotlin/com/dergoogler/mmrl/utils/timber/ReleaseTree.kt
@@ -1,9 +1,14 @@
 package com.dergoogler.mmrl.utils.timber
 
+import android.util.Log
 import timber.log.Timber
 
 class ReleaseTree : Timber.DebugTree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+            return
+        }
+
         super.log(priority, "<MR_REL>$tag", message, t)
     }
 }
