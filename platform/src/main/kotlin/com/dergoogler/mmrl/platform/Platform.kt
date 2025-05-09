e01218d4de17b2429b867d8b30388850ec567b04
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2025-05-09 12:19:07 +0200

    Update Platform.kt

--- platform/src/main/kotlin/com/dergoogler/mmrl/platform/Platform.kt
+++ platform/src/main/kotlin/com/dergoogler/mmrl/platform/Platform.kt
@@ -18,6 +18,7 @@ import com.dergoogler.mmrl.platform.hiddenApi.HiddenPackageManager
 import com.dergoogler.mmrl.platform.hiddenApi.HiddenUserManager
 import com.dergoogler.mmrl.platform.model.PlatformConfig
 import com.dergoogler.mmrl.platform.model.PlatformConfigImpl
+import com.dergoogler.mmrl.platform.model.PlatformState
 import com.dergoogler.mmrl.platform.stub.IFileManager
 import com.dergoogler.mmrl.platform.stub.IModuleManager
 import com.dergoogler.mmrl.platform.stub.IServiceManager
@@ -51,6 +52,9 @@ enum class Platform(val id: String) {
             }
 
         var isAlive by mutableStateOf(false)
+            private set
+
+        var state by mutableStateOf(PlatformState.UNKNOWN)
             private set
 
         private val _isAliveFlow = MutableStateFlow(false)
@@ -121,11 +125,11 @@ enum class Platform(val id: String) {
                         else -> null
                     }
 
-                    state()
+                    state(conf)
                 } catch (e: Exception) {
                     mServiceOrNull = null
                     Log.e(TAG, "Failed to init service manager", e)
-                    state()
+                    state(conf)
                 }
             }
         }
@@ -221,10 +225,10 @@ enum class Platform(val id: String) {
                 Platform.from(currentPlatform())
             }
 
-        private fun state(): Boolean {
+        private fun state(conf: PlatformConfigImpl): Boolean {
             isAlive = mServiceOrNull != null
             _isAliveFlow.value = isAlive
-
+            state = conf.state
             return isAlive
         }
 
