4e15fd6c89a953fa62dc529d45ed12b10f925d08
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2025-05-28 00:38:32 +0200

    Update Event.kt

--- webui/src/main/kotlin/com/dergoogler/mmrl/webui/model/Event.kt
+++ webui/src/main/kotlin/com/dergoogler/mmrl/webui/model/Event.kt
@@ -5,5 +5,5 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class WXEventHandler(
-    val type: PostWindowEventMessage
+    val type: PostWindowEventMessage? = null
 )
