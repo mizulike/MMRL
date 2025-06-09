0a408ebc5e6ac48b4034add70d28b358ecafb193
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2025-06-09 19:32:07 +0200

    ui: add switch docs

--- ui/src/main/kotlin/com/dergoogler/mmrl/ui/component/listItem/dsl/component/Switch.kt
+++ ui/src/main/kotlin/com/dergoogler/mmrl/ui/component/listItem/dsl/component/Switch.kt
@@ -1,5 +1,6 @@
 package com.dergoogler.mmrl.ui.component.listItem.dsl.component
 
+import androidx.compose.foundation.interaction.Interaction
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.material3.Switch
@@ -13,7 +14,24 @@ import com.dergoogler.mmrl.ui.component.listItem.dsl.ListScope
 import com.dergoogler.mmrl.ui.component.listItem.dsl.component.item.End
 
 /**
- * do not define a `ListItemScope.End` here as it already is with a Switch component
+ * A List item with a Switch component at the end.
+ *
+ * This component displays a list item with a switch on the right side.
+ * The entire item is toggleable. The `content` lambda defines the main
+ * content of the list item, which is placed to the left of the switch.
+ *
+ * **Note:** Do not define a `ListItemScope.End` within the `content` lambda,
+ * as this component automatically adds a Switch component in that position.
+ *
+ * @param checked Whether the switch is currently checked.
+ * @param enabled Whether the list item and switch are enabled.
+ * @param onChange Callback invoked when the checked state of the switch changes.
+ * @param interactionSource The [MutableInteractionSource] representing the stream of
+ *   [Interaction]s for this component. You can create and pass in your own `remember`ed
+ *   instance to observe [Interaction]s and customize the appearance / behavior of this
+ *   component in different [Interaction]s.
+ * @param content The composable content to display within the list item, to the left of the switch.
+ *   This will be placed in the `ListItemScope.Start` or `ListItemScope.Center` position implicitly.
  */
 @Composable
 fun ListScope.Switch(
