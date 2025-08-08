02d5de77833d3e46ea6a925e6c56e5415a809bd9
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2025-08-08 20:11:46 +0200

    Enable MagicMount and add action environment variables
    Set hasMagicMount to true in module compatibility and define getActionEnvironment to export relevant SukiSU environment variables for module actions.
    
    Co-Authored-By: WenHao2130 <WenHao2130@outlook.com>

--- platform/src/main/kotlin/com/dergoogler/mmrl/platform/manager/SukiSUModuleManager.kt
+++ platform/src/main/kotlin/com/dergoogler/mmrl/platform/manager/SukiSUModuleManager.kt
@@ -6,7 +6,15 @@ open class SukiSUModuleManager() : KernelSUModuleManager() {
     override fun getManagerName(): String = "SukiSU"
 
     override fun getModuleCompatibility() = ModuleCompatibility(
-        hasMagicMount = false,
+        hasMagicMount = true,
         canRestoreModules = false
+    )
+
+    override fun getActionEnvironment(): List<String> = listOf(
+        "export ASH_STANDALONE=1",
+        "export KSU=true",
+        "export KSU_SUKISU=true",
+        "export KSU_VER=${version}",
+        "export KSU_VER_CODE=${versionCode}",
     )
 }
